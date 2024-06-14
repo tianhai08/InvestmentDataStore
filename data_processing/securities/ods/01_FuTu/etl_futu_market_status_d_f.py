d9280091a45fa1bae4ba6431397252e4a3a6a859
tianhai08 <166911681+tianhai08@users.noreply.github.com>
2024-06-14 15:00:29 +0800

    Create etl_futu_market_status_d_f.py

---
+++ data_processing/securities/ods/01_FuTu/etl_futu_market_status_d_f.py
@@ -0,0 +1,9 @@
+from futu import *
+quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
+
+ret, data = quote_ctx.get_market_state(['SZ.000001', 'HK.00700'])
+if ret == RET_OK:
+    print(data)
+else:
+    print('error:', data)
+quote_ctx.close() # 结束后记得关闭当条连接，防止连接条数用尽
